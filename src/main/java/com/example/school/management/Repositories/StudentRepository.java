9af2e5bd3403e321b7a850500e2258e7cad76634
nidhal-98 <nidhalsalim98@gmail.com>
2023-05-23 10:13:45 +0400

    Creating StudentRepository Class in Repositories Package

---
+++ src/main/java/com/example/school/management/Repositories/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.example.school.management.Repositories;
+
+import com.example.school.management.Models.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+}
