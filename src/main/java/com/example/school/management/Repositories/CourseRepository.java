ecf30b5535ab8f6bd83edf3a6dad355a86ed2c96
nidhal-98 <nidhalsalim98@gmail.com>
2023-05-23 11:14:14 +0400

    Adding @Repository

--- src/main/java/com/example/school/management/Repositories/CourseRepository.java
+++ src/main/java/com/example/school/management/Repositories/CourseRepository.java
@@ -2,6 +2,8 @@ package com.example.school.management.Repositories;
 
 import com.example.school.management.Models.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
 }
