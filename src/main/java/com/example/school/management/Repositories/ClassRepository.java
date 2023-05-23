c323d50500261544acc924f709bb5d1a6fb1717c
nidhal-98 <nidhalsalim98@gmail.com>
2023-05-23 11:28:24 +0400

    Creating ClassRepository Class in Repositories Package

---
+++ src/main/java/com/example/school/management/Repositories/ClassRepository.java
@@ -0,0 +1,8 @@
+package com.example.school.management.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClassRepository extends JpaRepository<Class, Long> {
+}
