769b4bcaf2ed2074475cb124557740d70ce47cd6
nidhal-98 <nidhalsalim98@gmail.com>
2023-05-23 10:11:16 +0400

    Creating Student Class in Model Package

---
+++ src/main/java/com/example/school/management/Models/Student.java
@@ -0,0 +1,26 @@
+package com.example.school.management.Models;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long studentID;
+
+    String name;
+    String gender;
+    Integer age;
+
+
+
+}
