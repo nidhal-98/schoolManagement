88ee5a2617b7c5c2e10a0b19eb8b14f8d2965f3c
nidhal-98 <nidhalsalim98@gmail.com>
2023-05-23 10:16:06 +0400

    Creating StudentServices Class in Services Package

---
+++ src/main/java/com/example/school/management/Services/StudentServices.java
@@ -0,0 +1,19 @@
+package com.example.school.management.Services;
+
+import com.example.school.management.Models.Student;
+import com.example.school.management.Repositories.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentServices {
+
+    @Autowired
+    StudentRepository studentRepository;
+
+    public List<Student> getAllStudents(){
+        return studentRepository.findAll();
+    }
+}
