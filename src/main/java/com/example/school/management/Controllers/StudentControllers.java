d631d60f6630999dd459a18e7644df29d268434a
nidhal-98 <nidhalsalim98@gmail.com>
2023-05-23 11:55:43 +0400

    Adding @GetMapping("/getAllStudents") to get all students

--- src/main/java/com/example/school/management/Controllers/StudentControllers.java
+++ src/main/java/com/example/school/management/Controllers/StudentControllers.java
@@ -5,10 +5,7 @@ import com.example.school.management.Services.StudentServices;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -35,5 +32,10 @@ public class StudentControllers {
         "age" : 25
     }
     */
+
+    @GetMapping("/getAllStudents")
+    public List<Student> getAllStudents(){
+        return studentServices.getAllStudents();
+    }
 
 }
