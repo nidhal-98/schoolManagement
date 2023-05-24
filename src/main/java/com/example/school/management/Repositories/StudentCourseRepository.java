8be33f9d8f112c97feb34c1cf71a6d475af90776
nidhal-98 <nidhalsalim98@gmail.com>
2023-05-24 11:01:44 +0400

    Adding StudentCourseRepository in Repositories Package

---
+++ src/main/java/com/example/school/management/Repositories/StudentCourseRepository.java
@@ -0,0 +1,9 @@
+package com.example.school.management.Repositories;
+
+import com.example.school.management.Models.StudentCourse;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentCourseRepository extends JpaRepository<StudentCourse, Long> {
+}
