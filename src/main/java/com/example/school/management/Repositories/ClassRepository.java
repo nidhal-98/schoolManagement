a59c6e689eacdd9601b36d1526eeed9b5c1d6f99
nidhal-98 <nidhalsalim98@gmail.com>
2023-05-30 09:23:20 +0400

    Soft delete in ClassRepository

--- src/main/java/com/example/school/management/Repositories/ClassRepository.java
+++ src/main/java/com/example/school/management/Repositories/ClassRepository.java
@@ -1,9 +1,15 @@
 package com.example.school.management.Repositories;
 
 import com.example.school.management.Models.ClassRoom;
+import com.example.school.management.Models.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ClassRepository extends JpaRepository<ClassRoom, Long> {
+
+    List<Class> findAllByisActive(boolean active);
+
 }
